3c34f8ed167aea0db81f0ae0c292c309868ec94b
yuyu <1912850912@qq.com>
2019-07-25 10:46:12 +0800

    2019/7/25

--- components/shoye/tab/index.js
+++ components/shoye/tab/index.js
@@ -11,13 +11,33 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    currentTab: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+    //滑动切换
+    swiperTab: function(e) {
+      var that = this;
+      that.setData({
+        currentTba: e.detail.current
+      });
+    },
+    //点击切换
+    clickTab: function(e) {
 
-  }
+      var that = this;
+
+      if (this.data.currentTab === e.target.dataset.current) {
+        return false;
+      } else {
+        that.setData({
+          currentTab: e.target.dataset.current
+        })
+      }
+    }
+  },
+
 })
